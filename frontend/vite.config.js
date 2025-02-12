bb5d69192a59874f4d9e76008860cb562bd2e852
CodeSandbox <support@codesandbox.io>
2025-02-12 14:38:09 +0000

    Add GitHub Pages deployment configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ['pktlyd-5173.csb.app', 'localhost']
-  }
+  base: '/TextToAvatar/',
 })
